1249ddbb31e34571d8ff2498588ed29493c8ec5e
erlendnils1 <erlendnils1@gmail.com>
2019-09-09 09:31:03 +0200

    Fixed migration script

--- src/main/resources/db/migration/V4__Increased_text_column_length.sql
+++ src/main/resources/db/migration/V4__Increased_text_column_length.sql
@@ -1,10 +1,10 @@
-ALTER TABLE ONLY notice ALTER COLUMN text character varying(4000) NOT NULL,
+ALTER TABLE notice ALTER COLUMN text TYPE character varying(4000);
 
-ALTER TABLE ONLY contact ALTER COLUMN further_details character varying(4000) NOT NULL,
+ALTER TABLE contact ALTER COLUMN further_details TYPE character varying(4000);
 
-ALTER TABLE ONLY flexible_line ALTER COLUMN description character varying(4000) NOT NULL,
-ALTER TABLE ONLY flexible_stop_place ALTER COLUMN description character varying(4000) NOT NULL,
-ALTER TABLE ONLY service_journey ALTER COLUMN description character varying(4000) NOT NULL,
-ALTER TABLE ONLY network ALTER COLUMN description character varying(4000) NOT NULL,
-ALTER TABLE ONLY journey_pattern ALTER COLUMN description character varying(4000) NOT NULL,
+ALTER TABLE flexible_line ALTER COLUMN description TYPE character varying(4000);
+ALTER TABLE flexible_stop_place ALTER COLUMN description TYPE character varying(4000);
+ALTER TABLE service_journey ALTER COLUMN description TYPE character varying(4000);
+ALTER TABLE network ALTER COLUMN description TYPE character varying(4000);
+ALTER TABLE journey_pattern ALTER COLUMN description TYPE character varying(4000);
 
