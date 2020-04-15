aa35b496e93242230b96c933592b99c65dd4609b
Tom Erik Støwer <testower@gmail.com>
2020-04-15 13:30:25 +0200

    Fix resource naming

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,7 +28,7 @@ resource "google_project_iam_member" "uttu_pubsub_iam_member" {
   member = "serviceAccount:${google_service_account.uttu_service_account.email}"
 }
 
-resource "google_project_iam_member" "uttu_pubsub_iam_member" {
+resource "google_project_iam_member" "uttu_blobstore_iam_member" {
   project = var.gcp_project
   role    = var.service_account_blobstore_role
   member = "serviceAccount:${google_service_account.uttu_service_account.email}"
