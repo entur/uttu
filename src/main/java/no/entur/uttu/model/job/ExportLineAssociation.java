e61fd4e7360a0103d608470638f629c77edf4595
Tom Erik Støwer <testower@gmail.com>
2020-10-28 22:45:49 +0100

    WTK-222 Remove unused import

--- src/main/java/no/entur/uttu/model/job/ExportLineAssociation.java
+++ src/main/java/no/entur/uttu/model/job/ExportLineAssociation.java
@@ -4,7 +4,6 @@ import no.entur.uttu.model.Line;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
