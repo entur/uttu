bb05b4c71a6ad5ef4b4d49d6818ab9fadd154be6
vpaturet <46598384+vpaturet@users.noreply.github.com>
2021-03-10 12:04:49 +0100

    Remove keycloak dependencies (#79)

--- src/main/java/no/entur/uttu/config/OAuth2Config.java
+++ src/main/java/no/entur/uttu/config/OAuth2Config.java
@@ -16,18 +16,14 @@
 
 package no.entur.uttu.config;
 
-import org.entur.oauth2.AuthorizedWebClientBuilder;
 import org.entur.oauth2.JwtRoleAssignmentExtractor;
 import org.entur.oauth2.MultiIssuerAuthenticationManagerResolver;
 import org.entur.oauth2.RorAuth0RolesClaimAdapter;
 import org.rutebanken.helper.organisation.RoleAssignmentExtractor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.web.reactive.function.client.ExchangeStrategies;
-import org.springframework.web.reactive.function.client.WebClient;
 
 /**
  * Configure Spring Beans for OAuth2 resource server and OAuth2 client security.
