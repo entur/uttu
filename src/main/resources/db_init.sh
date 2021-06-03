bce4d2bd66bd35b961f0e471e72d330fd8e01e46
Tom Erik Støwer <testower@gmail.com>
2021-06-03 12:59:04 +0200

    WTK-219 Add migration to init script

--- src/main/resources/db_init.sh
+++ src/main/resources/db_init.sh
@@ -9,6 +9,7 @@ for f in ./db/migration/V1__Base_version.sql \
            ./db/migration/V7__Add_export_line_associations.sql \
            ./db/migration/V8__On_delete_cascade_line_fkey_in_export_line_association.sql \
            ./db/migration/V9__Add_key_list.sql \
+           ./db/migration/V10__Drop_fromDate_to_Date_on_Export.sql \
            ./import.sql
 do
   echo "Running migration for ${f}"
