8727fd6aff9875d301993d31d4da0077bfda4947
vpaturet <46598384+vpaturet@users.noreply.github.com>
2021-08-24 13:15:55 +0200

    WTK-497 Remove support for Keycloak (#87)

--- src/main/java/no/entur/uttu/config/OAuth2Config.java
+++ src/main/java/no/entur/uttu/config/OAuth2Config.java
@@ -17,13 +17,14 @@
 package no.entur.uttu.config;
 
 import org.entur.oauth2.JwtRoleAssignmentExtractor;
-import org.entur.oauth2.MultiIssuerAuthenticationManagerResolver;
-import org.entur.oauth2.RorAuth0RolesClaimAdapter;
+import org.entur.oauth2.RoRJwtDecoderBuilder;
 import org.rutebanken.helper.organisation.RoleAssignmentExtractor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 /**
  * Configure Spring Beans for OAuth2 resource server and OAuth2 client security.
@@ -43,46 +44,21 @@ public class OAuth2Config {
     }
 
     /**
-     * Adapt the JWT claims produced by the RoR Auth0 tenant to make them compatible with those produced by Keycloak.
+     * Build a @{@link JwtDecoder} for RoR Auth0 domain.
      *
-     * @param rorAuth0ClaimNamespace
-     * @return
+     * @return a @{@link JwtDecoder} for Auth0.
      */
     @Bean
-    public RorAuth0RolesClaimAdapter rorAuth0RolesClaimAdapter(@Value("${uttu.oauth2.resourceserver.auth0.ror.claim.namespace}") String rorAuth0ClaimNamespace) {
-        return new RorAuth0RolesClaimAdapter(rorAuth0ClaimNamespace);
-    }
+    public JwtDecoder rorAuth0JwtDecoder(OAuth2ResourceServerProperties properties,
+                                         @Value("${uttu.oauth2.resourceserver.auth0.ror.jwt.audience}") String rorAuth0Audience,
+                                         @Value("${uttu.oauth2.resourceserver.auth0.ror.claim.namespace}") String rorAuth0ClaimNamespace) {
 
-    /**
-     * Identify the issuer of the JWT token (Auth0 or Keycloak) and forward the JWT token to the corresponding JWT decoder.
-     * Verify that the audience is valid and adapt the JWT claim using the injected claim adapter.
-     *
-     * @param keycloakAudience
-     * @param keycloakIssuer
-     * @param keycloakJwksetUri
-     * @param rorAuth0Audience
-     * @param rorAuth0Issuer
-     * @param rorAuth0RolesClaimAdapter
-     * @return
-     */
-    @Bean
-    public MultiIssuerAuthenticationManagerResolver multiIssuerAuthenticationManagerResolver(@Value("${uttu.oauth2.resourceserver.keycloak.jwt.audience}") String keycloakAudience,
-                                                                                             @Value("${uttu.oauth2.resourceserver.keycloak.jwt.issuer-uri}") String keycloakIssuer,
-                                                                                             @Value("${uttu.oauth2.resourceserver.keycloak.jwt.jwkset-uri}") String keycloakJwksetUri,
-                                                                                             @Value("${uttu.oauth2.resourceserver.auth0.ror.jwt.audience}") String rorAuth0Audience,
-                                                                                             @Value("${uttu.oauth2.resourceserver.auth0.ror.jwt.issuer-uri}") String rorAuth0Issuer,
-                                                                                             RorAuth0RolesClaimAdapter rorAuth0RolesClaimAdapter) {
-        return new MultiIssuerAuthenticationManagerResolver.Builder()
-                .withKeycloakAudience(keycloakAudience)
-                .withKeycloakIssuer(keycloakIssuer)
-                .withKeycloakJwksetUri(keycloakJwksetUri)
-                .withRorAuth0Audience(rorAuth0Audience)
-                .withRorAuth0Issuer(rorAuth0Issuer)
-                .withRorAuth0RolesClaimAdapter(rorAuth0RolesClaimAdapter)
+        String rorAuth0Issuer = properties.getJwt().getIssuerUri();
+        return new RoRJwtDecoderBuilder().withIssuer(rorAuth0Issuer)
+                .withAudience(rorAuth0Audience)
+                .withAuth0ClaimNamespace(rorAuth0ClaimNamespace)
                 .build();
-
     }
-
 
 }
 
