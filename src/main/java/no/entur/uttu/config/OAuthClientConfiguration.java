1a28f4df7599c9f8c78988920b4a7f69917b8792
Tom Erik Støwer <testower@gmail.com>
2022-04-29 21:16:23 +0200

    Exclude client configuraiton from test

--- src/main/java/no/entur/uttu/config/OAuthClientConfiguration.java
+++ src/main/java/no/entur/uttu/config/OAuthClientConfiguration.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
+@Profile("!test")
 public class OAuthClientConfiguration {
     @Bean
     WebClient webClient(WebClient.Builder webClientBuilder, OAuth2ClientProperties properties, @Value("${orgRegister.oauth2.client.audience}") String audience) {
