2858cb38a59296d2f512c8038b356a781b10b85c
Tom Erik Støwer <testower@gmail.com>
2023-01-11 14:56:20 +0100

    Revert db migration

--- src/main/resources/db/migration/V14__Use_time_no_timestamp_in_timetable.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE timetabled_passing_time
-    ALTER COLUMN arrival_time TYPE time without time zone,
-    ALTER COLUMN departure_time TYPE time without time zone,
-    ALTER COLUMN latest_arrival_time TYPE time without time zone,
-    ALTER COLUMN earliest_departure_time TYPE time without time zone;
