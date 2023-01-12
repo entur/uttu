6ffb3e525f41e6f0928e2681f09bcd12d3cc408f
Tom Erik Støwer <testower@gmail.com>
2023-01-12 11:20:12 +0100

    Fix db type for booking_arrangement.latest_booking_time

--- src/main/resources/db/migration/V14__Use_time_no_timestamp_in_timetable.sql
+++ src/main/resources/db/migration/V14__Use_time_no_timestamp_in_timetable.sql
@@ -3,3 +3,6 @@ ALTER TABLE timetabled_passing_time
     ALTER COLUMN departure_time TYPE time without time zone,
     ALTER COLUMN latest_arrival_time TYPE time without time zone,
     ALTER COLUMN earliest_departure_time TYPE time without time zone;
+
+ALTER TABLE booking_arrangement
+    ALTER COLUMN latest_booking_time TYPE time without time zone;
