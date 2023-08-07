509af8f02d52b762fd28ae64a0e79cc758b4f12f
Tom Erik Støwer <testower@gmail.com>
2023-08-07 20:51:44 +0200

    Add data migration

--- src/main/resources/db/migration/V16__Flexible_stop_place_flexible_areas_one_to_many.sql
+++ src/main/resources/db/migration/V16__Flexible_stop_place_flexible_areas_one_to_many.sql
@@ -1,2 +1,7 @@
 ALTER TABLE flexible_area ADD COLUMN  flexible_stop_place_pk bigint;
+
+UPDATE flexible_area SET flexible_stop_place_pk = flexible_stop_place.pk
+                    FROM flexible_stop_place
+                    WHERE flexible_area.pk = flexible_stop_place.flexible_area_pk;
+
 ALTER TABLE flexible_stop_place DROP COLUMN flexible_area_pk;
