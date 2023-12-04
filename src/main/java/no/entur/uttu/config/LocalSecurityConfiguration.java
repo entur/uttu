7d03c031c6124ed9f7ff813dc7447de6b72a86b5
Tom Erik Støwer <testower@gmail.com>
2023-12-04 12:57:20 +0100

    Remove unused import

--- src/main/java/no/entur/uttu/config/LocalSecurityConfiguration.java
+++ src/main/java/no/entur/uttu/config/LocalSecurityConfiguration.java
@@ -6,7 +6,6 @@ import org.rutebanken.helper.organisation.RoleAssignmentExtractor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
