a7d3ae4c7367d4e84864fd0a88371eb8cc66b294
Vincent Paturet <vincent.paturet@entur.org>
2024-05-06 11:36:01 +0200

    Fix test security configuration

--- src/test/java/no/entur/uttu/config/SecurityConfiguration.java
+++ src/test/java/no/entur/uttu/config/SecurityConfiguration.java
@@ -3,9 +3,9 @@ package no.entur.uttu.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -21,19 +21,20 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
   @Bean
-  public SecurityFilterChain filterChain(
-    HttpSecurity http,
-    AuthenticationManagerBuilder authBuilder
-  ) throws Exception {
-    authBuilder.userDetailsService(inMemoryUserDetailsManager());
-
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     return http
       .csrf(AbstractHttpConfigurer::disable)
       .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
       .formLogin(Customizer.withDefaults())
       .httpBasic(Customizer.withDefaults())
-      .authenticationManager(authBuilder.build())
       .build();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(
+    AuthenticationConfiguration authenticationConfiguration
+  ) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
   }
 
   @Bean
