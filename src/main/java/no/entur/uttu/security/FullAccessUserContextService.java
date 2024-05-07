8c98e836224ca4d282adc687e40626d6f82b56bb
Tom Erik Støwer <tom.erik.stower@entur.org>
2024-05-07 08:26:35 +0200

    Add some javadoc1

--- src/main/java/no/entur/uttu/security/FullAccessUserContextService.java
+++ src/main/java/no/entur/uttu/security/FullAccessUserContextService.java
@@ -1,5 +1,9 @@
 package no.entur.uttu.security;
 
+/**
+ * Fallback implementation giving full access to all operations for authenticated users,
+ * enable by setting property uttu.security.user-context-service=full-access
+ */
 public class FullAccessUserContextService implements UserContextService {
 
   @Override
--- src/main/java/no/entur/uttu/security/UserContextService.java
+++ src/main/java/no/entur/uttu/security/UserContextService.java
@@ -1,7 +1,24 @@
 package no.entur.uttu.security;
 
+/**
+ *  This interface provides authorization features to the app
+ */
 public interface UserContextService {
+
+  /**
+   * Get the user's display name
+   */
   String getPreferredName();
+
+  /**
+   * Is the user an administrator?
+   */
   boolean isAdmin();
+
+  /**
+   * Whether the user has access to edit data for a provider
+   * @param providerCode The (codespace) code of the provider
+   * @return true if the user has access
+   */
   boolean hasAccessToProvider(String providerCode);
 }
