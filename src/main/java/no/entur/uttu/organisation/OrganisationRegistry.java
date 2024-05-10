99bbbb773fd1375a146370b9a6aeb9af19a0ab29
Tom Erik Støwer <tom.erik.stower@entur.org>
2024-05-10 17:08:42 +0200

    Add javadoc to interface

--- src/main/java/no/entur/uttu/organisation/OrganisationRegistry.java
+++ src/main/java/no/entur/uttu/organisation/OrganisationRegistry.java
@@ -2,36 +2,37 @@ package no.entur.uttu.organisation;
 
 import java.util.List;
 import java.util.Optional;
+
+import no.entur.uttu.error.codedexception.CodedIllegalArgumentException;
 import org.rutebanken.netex.model.GeneralOrganisation;
 
 /**
- *
+ * Represents an organisation registry used to populate authorities and operators references
  */
 public interface OrganisationRegistry {
   /**
-   *
-   * @return
+   * Get a list of all organisations in the registry
    */
   List<GeneralOrganisation> getOrganisations();
 
   /**
-   *
-   * @param id
-   * @return
+   * Get an organisation with the given ID, which may not exist
    */
   Optional<GeneralOrganisation> getOrganisation(String id);
 
   /**
-   *
-   * @param operatorRef
-   * @return
+   * Check if the organisation represented by the operator reference id is a valid operator
+   * @param operatorRef The organisation id
+   * @return The id if it is a valid operator
+   * @throws CodedIllegalArgumentException if the organisation is not a valid operator
    */
   String getVerifiedOperatorRef(String operatorRef);
 
   /**
-   *
-   * @param authorityRef
-   * @return
+   * Check if the organisation represented by the operator reference id is a valid authority
+   * @param authorityRef The organisation id
+   * @return The id if it is a valid authority
+   * @throws CodedIllegalArgumentException if the organisation is not a valid authority
    */
   String getVerifiedAuthorityRef(String authorityRef);
 }
