5e3d43e71936048de299e6a2799f440a2c322164
Tom Erik Støwer <tom.erik.stower@entur.org>
2024-10-04 19:15:25 +0200

    Remove unused import

--- src/main/java/no/entur/uttu/export/netex/producer/common/OrganisationProducer.java
+++ src/main/java/no/entur/uttu/export/netex/producer/common/OrganisationProducer.java
@@ -16,7 +16,6 @@
 package no.entur.uttu.export.netex.producer.common;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
