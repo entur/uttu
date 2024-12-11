46ad6a720b8b81450a0348d30e18a2ec1ba2d767
Tom Erik Støwer <tom.erik.stower@entur.org>
2024-12-11 13:05:08 +0100

    fix: when creating an id for a VersionedChildStructure entity, the _VersionedChildStructure suffix should be stripped

---
+++ src/test/java/no/entur/uttu/export/netex/producer/NetexObjectFactoryTest.java
@@ -0,0 +1,27 @@
+package no.entur.uttu.export.netex.producer;
+
+import no.entur.uttu.config.ExportTimeZone;
+import no.entur.uttu.model.Ref;
+import no.entur.uttu.util.DateUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.rutebanken.netex.model.ServiceLinkInJourneyPattern_VersionedChildStructure;
+
+public class NetexObjectFactoryTest {
+
+    @Test
+    void populateIdReturnsValidIdForVersionedChildStructureElements() {
+        NetexObjectFactory factory = new NetexObjectFactory(
+                new DateUtils(),
+                new ExportTimeZone()
+        );
+
+        Assertions.assertEquals(
+                "ENT:ServiceLinkInJourneyPattern:1",
+                factory.populateId(
+                        new ServiceLinkInJourneyPattern_VersionedChildStructure(),
+                        new Ref("ENT:ServiceLink:1", "1")
+                ).getId()
+        );
+    }
+}
