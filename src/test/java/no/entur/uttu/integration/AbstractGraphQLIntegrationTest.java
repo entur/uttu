5e0ff348e8a59570604226ae71bb3675c899d3eb
Tom Erik Støwer <tom.erik.stower@entur.org>
2025-01-29 23:09:00 +0100

    Reimplement groovy network graphql integration test in java

---
+++ src/test/java/no/entur/uttu/integration/AbstractGraphQLIntegrationTest.java
@@ -0,0 +1,36 @@
+package no.entur.uttu.integration;
+
+import no.entur.uttu.UttuIntegrationTest;
+import no.entur.uttu.stubs.UserContextServiceStub;
+import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.test.tester.HttpGraphQlTester;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@ActiveProfiles({ "in-memory-blobstore" })
+public abstract class AbstractGraphQLIntegrationTest extends UttuIntegrationTest {
+
+  @Autowired
+  private UserContextServiceStub userContextServiceStub;
+
+  HttpGraphQlTester graphQlTester;
+
+  @Before
+  public void setup() {
+    userContextServiceStub.setPreferredName("John Doe");
+    userContextServiceStub.setAdmin(false);
+    userContextServiceStub.setHasAccessToProvider("tst", true);
+    userContextServiceStub.setHasAccessToProvider("foo", false);
+
+    WebTestClient.Builder clientBuilder = WebTestClient
+      .bindToServer()
+      .baseUrl("http://localhost:" + port + "/services/flexible-lines/tst/graphql");
+
+    graphQlTester =
+      HttpGraphQlTester
+        .builder(clientBuilder)
+        .headers(headers -> headers.setBasicAuth("admin", "topsecret"))
+        .build();
+  }
+}
