62548121f782b6792f414cfeec96a6b2cee084cb
Tom Erik Støwer <tom.erik.stower@entur.org>
2025-02-04 10:47:06 +0100

    Remove todos, no lang attribute needed

--- src/main/java/no/entur/uttu/export/netex/producer/common/BrandingProducer.java
+++ src/main/java/no/entur/uttu/export/netex/producer/common/BrandingProducer.java
@@ -3,7 +3,6 @@ package no.entur.uttu.export.netex.producer.common;
 import java.util.List;
 import java.util.stream.Collectors;
 import no.entur.uttu.export.netex.NetexExportContext;
-import no.entur.uttu.export.netex.producer.NetexObjectFactory;
 import org.rutebanken.netex.model.Branding;
 import org.rutebanken.netex.model.MultilingualString;
 import org.springframework.stereotype.Component;
@@ -18,8 +17,8 @@ public class BrandingProducer {
         new Branding()
           .withId(branding.getNetexId())
           .withVersion(branding.getVersion().toString())
-          .withName(new MultilingualString().withValue(branding.getName())) // TODO lang?
-          .withShortName(new MultilingualString().withValue(branding.getShortName())) // TODO lang?
+          .withName(new MultilingualString().withValue(branding.getName()))
+          .withShortName(new MultilingualString().withValue(branding.getShortName()))
           .withDescription(new MultilingualString().withValue(branding.getDescription()))
           .withUrl(branding.getUrl())
           .withImage(branding.getImageUrl())
