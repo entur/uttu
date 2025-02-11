8381f898e1f04bd6c04eed4fe1f6c165fb566100
Tom Erik Støwer <tom.erik.stower@entur.org>
2025-02-11 09:20:56 +0100

    Extend configuration to support multiple endpoints

--- src/main/java/no/entur/uttu/routing/osrm/OsrmConfiguration.java
+++ src/main/java/no/entur/uttu/routing/osrm/OsrmConfiguration.java
@@ -1,5 +1,6 @@
 package no.entur.uttu.routing.osrm;
 
+import java.util.HashMap;
 import java.util.Map;
 import no.entur.uttu.routing.RoutingProfile;
 import no.entur.uttu.routing.RoutingService;
@@ -14,8 +15,34 @@ public class OsrmConfiguration {
 
   @Bean
   RoutingService routingService(
-    @Value("${uttu.routing.osrm-api}") String osrmApiEndpoint
+    @Value("${uttu.routing.osrm-api}") String osrmApiEndpoint,
+    @Value("${uttu.routing.osrm-api-bus}") String osrmApiBusEndpoint,
+    @Value("${uttu.routing.osrm-api-rail}") String osrmApiRailEndpoint,
+    @Value("${uttu.routing.osrm-api-water}") String osrmApiWaterEndpoint
   ) {
+    if (osrmApiEndpoint != null) {
+      return createStandaloneBusRoutingService(osrmApiEndpoint);
+    } else {
+      return createRoutingService(osrmApiBusEndpoint, osrmApiRailEndpoint, osrmApiWaterEndpoint);
+    }
+  }
+
+  private RoutingService createRoutingService(String osrmApiBusEndpoint, String osrmApiRailEndpoint, String osrmApiWaterEndpoint) {
+    var endpointMap = new HashMap<RoutingProfile, String>();
+
+    if (osrmApiBusEndpoint != null) {
+      endpointMap.put(RoutingProfile.BUS, osrmApiBusEndpoint);
+    }
+    if (osrmApiRailEndpoint != null) {
+      endpointMap.put(RoutingProfile.RAIL, osrmApiRailEndpoint);
+    }
+    if (osrmApiWaterEndpoint != null) {
+      endpointMap.put(RoutingProfile.WATER, osrmApiWaterEndpoint);
+    }
+    return new OsrmService(endpointMap);
+  }
+
+  private RoutingService createStandaloneBusRoutingService(String osrmApiEndpoint) {
     return new OsrmService(Map.of(RoutingProfile.BUS, osrmApiEndpoint));
   }
 }
