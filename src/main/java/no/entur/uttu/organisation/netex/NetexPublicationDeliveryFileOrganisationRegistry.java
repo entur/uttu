5710dfae9f541da0bdeeb28b93793e27c63dcffe
Tom Erik Støwer <tom.erik.stower@entur.org>
2025-03-12 09:26:57 +0100

    Improve

--- src/main/java/no/entur/uttu/organisation/netex/NetexPublicationDeliveryFileOrganisationRegistry.java
+++ src/main/java/no/entur/uttu/organisation/netex/NetexPublicationDeliveryFileOrganisationRegistry.java
@@ -1,14 +1,10 @@
 package no.entur.uttu.organisation.netex;
 
-import no.entur.uttu.netex.NetexUnmarshallerUnmarshalFromSourceException;
-import org.rutebanken.netex.model.Authority;
-import org.rutebanken.netex.model.Operator;
-import org.rutebanken.netex.model.PublicationDeliveryStructure;
-import org.rutebanken.netex.model.ResourceFrame;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
+import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 import java.io.File;
 
@@ -16,42 +12,12 @@ import java.io.File;
 @ConditionalOnProperty(
         name = "uttu.organisations.netex-file-uri"
 )
-public class NetexPublicationDeliveryFileOrganisationRegistry extends NetexPublicationDeliveryOrganisationRegistry{
+public class NetexPublicationDeliveryFileOrganisationRegistry extends NetexPublicationDeliveryOrganisationRegistry {
     @Value("${uttu.organisations.netex-file-uri}")
     String netexFileUri;
 
     @Override
-    public void init() {
-        try {
-            PublicationDeliveryStructure publicationDeliveryStructure =
-                    netexUnmarshaller.unmarshalFromSource(new StreamSource(new File(netexFileUri)));
-            publicationDeliveryStructure
-                    .getDataObjects()
-                    .getCompositeFrameOrCommonFrame()
-                    .forEach(frame -> {
-                        var frameValue = frame.getValue();
-                        if (frameValue instanceof ResourceFrame resourceFrame) {
-                            resourceFrame
-                                    .getOrganisations()
-                                    .getOrganisation_()
-                                    .forEach(org -> {
-                                        if (org.getDeclaredType().isAssignableFrom(Authority.class)) {
-                                            authorities.add((Authority) org.getValue());
-                                        } else if (org.getDeclaredType().isAssignableFrom(Operator.class)) {
-                                            operators.add((Operator) org.getValue());
-                                        } else {
-                                            throw new RuntimeException(
-                                                    "Unsupported organisation type: " + org.getDeclaredType()
-                                            );
-                                        }
-                                    });
-                        }
-                    });
-        } catch (NetexUnmarshallerUnmarshalFromSourceException e) {
-            logger.warn(
-                    "Unable to unmarshal organisations xml, organisation registry will be an empty list",
-                    e
-            );
-        }
+    public Source getPublicationDeliverySource() {
+        return new StreamSource(new File(netexFileUri));
     }
 }
--- src/main/java/no/entur/uttu/organisation/netex/NetexPublicationDeliveryOrganisationRegistry.java
+++ src/main/java/no/entur/uttu/organisation/netex/NetexPublicationDeliveryOrganisationRegistry.java
@@ -1,18 +1,24 @@
 package no.entur.uttu.organisation.netex;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import javax.annotation.PostConstruct;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+
 import no.entur.uttu.error.codederror.CodedError;
 import no.entur.uttu.error.codes.ErrorCodeEnumeration;
 import no.entur.uttu.netex.NetexUnmarshaller;
+import no.entur.uttu.netex.NetexUnmarshallerUnmarshalFromSourceException;
 import no.entur.uttu.organisation.spi.OrganisationRegistry;
 import no.entur.uttu.util.Preconditions;
 import org.rutebanken.netex.model.Authority;
 import org.rutebanken.netex.model.Operator;
 import org.rutebanken.netex.model.PublicationDeliveryStructure;
+import org.rutebanken.netex.model.ResourceFrame;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,7 +42,41 @@ public abstract class NetexPublicationDeliveryOrganisationRegistry
   );
 
   @PostConstruct
-  public abstract void init();
+  public void init() {
+    try {
+      PublicationDeliveryStructure publicationDeliveryStructure =
+              netexUnmarshaller.unmarshalFromSource(getPublicationDeliverySource());
+      publicationDeliveryStructure
+              .getDataObjects()
+              .getCompositeFrameOrCommonFrame()
+              .forEach(frame -> {
+                var frameValue = frame.getValue();
+                if (frameValue instanceof ResourceFrame resourceFrame) {
+                  resourceFrame
+                          .getOrganisations()
+                          .getOrganisation_()
+                          .forEach(org -> {
+                            if (org.getDeclaredType().isAssignableFrom(Authority.class)) {
+                              authorities.add((Authority) org.getValue());
+                            } else if (org.getDeclaredType().isAssignableFrom(Operator.class)) {
+                              operators.add((Operator) org.getValue());
+                            } else {
+                              throw new RuntimeException(
+                                      "Unsupported organisation type: " + org.getDeclaredType()
+                              );
+                            }
+                          });
+                }
+              });
+    } catch (NetexUnmarshallerUnmarshalFromSourceException e) {
+      logger.warn(
+              "Unable to unmarshal organisations xml, organisation registry will be an empty list",
+              e
+      );
+    }
+  }
+
+  public abstract Source getPublicationDeliverySource();
 
   @Override
   public List<Authority> getAuthorities() {
