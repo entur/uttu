a5a1236ca3a36783838e8658afd7aec5df3de1b2
Tom Erik Støwer <tom.erik.stower@entur.org>
2025-03-12 21:44:13 +0100

    Improve test

--- src/test/java/no/entur/uttu/organisation/netex/NetexPublicationDeliveryOrganisationRegistryTest.java
+++ src/test/java/no/entur/uttu/organisation/netex/NetexPublicationDeliveryOrganisationRegistryTest.java
@@ -3,18 +3,13 @@ package no.entur.uttu.organisation.netex;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class NetexPublicationDeliveryOrganisationRegistryTest {
+class NetexPublicationDeliveryOrganisationRegistryTest {
 
   @Test
   void testUnsupportedOrganisationTypeThrows() {
-    Assertions.assertThrows(
-      UnsupportedOrganisationTypeException.class,
-      () -> {
-        new NetexPublicationDeliveryFileOrganisationRegistry(
-          "src/test/resources/organisation/netex/unsupported_org_type.xml"
-        )
-          .init();
-      }
+    var registry = new NetexPublicationDeliveryFileOrganisationRegistry(
+      "src/test/resources/organisation/netex/unsupported_org_type.xml"
     );
+    Assertions.assertThrows(UnsupportedOrganisationTypeException.class, registry::init);
   }
 }
