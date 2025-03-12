efbafe543a5dfc159e466e2529e7df6f2a6ad409
Tom Erik Støwer <tom.erik.stower@entur.org>
2025-03-12 21:57:43 +0100

    Add test for normal organisations file

--- src/test/java/no/entur/uttu/organisation/netex/NetexPublicationDeliveryOrganisationRegistryTest.java
+++ src/test/java/no/entur/uttu/organisation/netex/NetexPublicationDeliveryOrganisationRegistryTest.java
@@ -6,6 +6,16 @@ import org.junit.jupiter.api.Test;
 class NetexPublicationDeliveryOrganisationRegistryTest {
 
   @Test
+  void testCanQueryOrganisations() {
+    var registry = new NetexPublicationDeliveryFileOrganisationRegistry(
+      "src/test/resources/fixtures/organisations.xml"
+    );
+    registry.init();
+    Assertions.assertEquals(1, registry.getAuthorities().size());
+    Assertions.assertEquals(1, registry.getOperators().size());
+  }
+
+  @Test
   void testUnsupportedOrganisationTypeThrows() {
     var registry = new NetexPublicationDeliveryFileOrganisationRegistry(
       "src/test/resources/organisation/netex/unsupported_org_type.xml"
