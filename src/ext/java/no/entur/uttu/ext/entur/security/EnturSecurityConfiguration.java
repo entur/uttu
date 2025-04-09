0d53492d1a5d0500ba0b2951576773408320dce8
Vincent Paturet <vincent.paturet@entur.org>
2025-04-09 15:51:42 +0200

    Add support for Permission Store proxy

--- src/ext/java/no/entur/uttu/ext/entur/security/EnturSecurityConfiguration.java
+++ src/ext/java/no/entur/uttu/ext/entur/security/EnturSecurityConfiguration.java
@@ -3,18 +3,24 @@ package no.entur.uttu.ext.entur.security;
 import jakarta.servlet.http.HttpServletRequest;
 import no.entur.uttu.repository.ProviderRepository;
 import no.entur.uttu.security.spi.UserContextService;
+import org.entur.oauth2.AuthorizedWebClientBuilder;
 import org.entur.oauth2.JwtRoleAssignmentExtractor;
 import org.entur.oauth2.RorAuthenticationConverter;
 import org.entur.oauth2.multiissuer.MultiIssuerAuthenticationManagerResolverBuilder;
 import org.entur.oauth2.user.JwtUserInfoExtractor;
+import org.entur.ror.permission.RemoteBabaRoleAssignmentExtractor;
 import org.rutebanken.helper.organisation.RoleAssignmentExtractor;
 import org.rutebanken.helper.organisation.user.UserInfoExtractor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManagerResolver;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 @Profile("entur")
@@ -25,9 +31,52 @@ public class EnturSecurityConfiguration {
     return new RorAuthenticationConverter();
   }
 
+  @ConditionalOnProperty(
+    value = "entur.security.role.assignment.extractor",
+    havingValue = "jwt",
+    matchIfMissing = true
+  )
   @Bean
-  public RoleAssignmentExtractor roleAssignmentExtractor() {
+  public RoleAssignmentExtractor jwtRoleAssignmentExtractor() {
     return new JwtRoleAssignmentExtractor();
+  }
+
+  @ConditionalOnProperty(
+    value = "entur.security.role.assignment.extractor",
+    havingValue = "baba"
+  )
+  @Bean
+  public RoleAssignmentExtractor babaRoleAssignmentExtractor(
+    @Qualifier("internalWebClient") WebClient webClient,
+    @Value("${entur.permission.rest.service.url}") String url
+  ) {
+    return new RemoteBabaRoleAssignmentExtractor(webClient, url);
+  }
+
+  /**
+   * Return a WebClient for authorized API calls.
+   * The WebClient inserts a JWT bearer token in the Authorization HTTP header.
+   * The JWT token is obtained from the configured Authorization Server.
+   *
+   * @param properties The spring.security.oauth2.client.registration.* properties
+   * @param audience   The API audience, required for obtaining a token from Auth0
+   * @return a WebClient for authorized API calls.
+   */
+  @ConditionalOnProperty(
+    value = "entur.security.role.assignment.extractor",
+    havingValue = "baba"
+  )
+  @Bean("internalWebClient")
+  WebClient internalWebClient(
+    WebClient.Builder webClientBuilder,
+    OAuth2ClientProperties properties,
+    @Value("${internal.oauth2.client.audience}") String audience
+  ) {
+    return new AuthorizedWebClientBuilder(webClientBuilder)
+      .withOAuth2ClientProperties(properties)
+      .withAudience(audience)
+      .withClientRegistrationId("internal")
+      .build();
   }
 
   @Bean
