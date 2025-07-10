0f76eafde597c004ca9f16f672743dcb0a1fbe86
Vincent Paturet <vincent.paturet@entur.org>
2025-07-10 09:01:31 +0200

    Use UserInfoExtractor for PubSub message

--- src/test/java/no/entur/uttu/config/SecurityConfiguration.java
+++ src/test/java/no/entur/uttu/config/SecurityConfiguration.java
@@ -1,7 +1,10 @@
 package no.entur.uttu.config;
 
+import javax.annotation.Nullable;
+import no.entur.uttu.security.UserInfoFilter;
 import no.entur.uttu.security.spi.UserContextService;
 import no.entur.uttu.stubs.UserContextServiceStub;
+import org.rutebanken.helper.organisation.user.UserInfoExtractor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,6 +17,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @Profile("test")
@@ -27,7 +31,25 @@ public class SecurityConfiguration {
       .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
       .formLogin(Customizer.withDefaults())
       .httpBasic(Customizer.withDefaults())
+      .addFilterAfter(
+        new UserInfoFilter(userInfoExtractor()),
+        UsernamePasswordAuthenticationFilter.class
+      )
       .build();
+  }
+
+  private UserInfoExtractor userInfoExtractor() {
+    return new UserInfoExtractor() {
+      @Override
+      public @Nullable String getPreferredName() {
+        return null;
+      }
+
+      @Override
+      public @Nullable String getPreferredUsername() {
+        return null;
+      }
+    };
   }
 
   @Bean
