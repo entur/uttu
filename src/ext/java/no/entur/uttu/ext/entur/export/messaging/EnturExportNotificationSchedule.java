9cd4ad170cc8f642565ed9edbfc450cac7bbad61
Tom Erik Støwer <tom.erik.stower@entur.org>
2025-11-07 15:03:09 +0100

    Add doc

--- src/ext/java/no/entur/uttu/ext/entur/export/messaging/EnturExportNotificationSchedule.java
+++ src/ext/java/no/entur/uttu/ext/entur/export/messaging/EnturExportNotificationSchedule.java
@@ -6,6 +6,20 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 
+/**
+ * This class will send an export notification to the messaging service
+ * for the latest export of each provider at the configured schedule.
+ *
+ * <p>
+ *   <b>Note: should only be used with a single instance. If you're using
+ *   multiple instances, a notification will be sent from each instance!</b>
+ * </p>
+ *
+ * To enable set property
+ *    entur.export.notification.schedule.enabled=true
+ * To override default schedule use this property
+ *    entur.export.notification.schedule.cron=0 30 4 * * *
+ */
 @Configuration
 @ConditionalOnProperty(
   value = "entur.export.notification.schedule.enabled",
