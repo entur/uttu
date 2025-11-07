fda41851b3b896a7708dbaa916ba12343359c5e7
Tom Erik Støwer <tom.erik.stower@entur.org>
2025-11-07 14:58:19 +0100

    Send notifications for all exports

--- src/ext/java/no/entur/uttu/ext/entur/export/messaging/EnturExportNotificationSchedule.java
+++ src/ext/java/no/entur/uttu/ext/entur/export/messaging/EnturExportNotificationSchedule.java
@@ -1,41 +1,34 @@
-package no.entur.uttu.ext.entur.synthetictestdata;
+package no.entur.uttu.ext.entur.export.messaging;
 
 import no.entur.uttu.export.messaging.spi.MessagingService;
 import no.entur.uttu.repository.ExportRepository;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
 @ConditionalOnProperty(
-  value = "entur.synthetictestdata.schedule.enabled",
+  value = "entur.export.notification.schedule.enabled",
   havingValue = "true"
 )
-public class SyntheticTestDataExportNotificationSchedule {
+public class EnturExportNotificationSchedule {
 
   private final MessagingService messagingService;
   private final ExportRepository exportRepository;
-  private final String codespace;
 
-  public SyntheticTestDataExportNotificationSchedule(
+  public EnturExportNotificationSchedule(
     MessagingService messagingService,
-    ExportRepository exportRepository,
-    @Value("${entur.synthetictestdata.schedule.codespace:aas}") String codespace
+    ExportRepository exportRepository
   ) {
     this.messagingService = messagingService;
     this.exportRepository = exportRepository;
-    this.codespace = codespace;
   }
 
-  @Scheduled(cron = "${entur.synthetictestdata.schedule.cron:0 0 2 * * *}")
+  @Scheduled(cron = "${entur.export.notification.schedule.cron:0 0 2 * * *}")
   public void schedule() {
     exportRepository
       .getLatestExportByProviders()
-      .stream()
-      .filter(export -> export.getProvider().getCode().equals(codespace))
-      .findFirst()
-      .ifPresent(
+      .forEach(
         export ->
           messagingService.notifyExport(
             export.getProvider().getCode(),
