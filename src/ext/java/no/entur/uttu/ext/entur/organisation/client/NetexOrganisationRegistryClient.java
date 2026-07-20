0d4a9605345e4f46b2e5ad20a2a3a598dd6c4be2
Gunnar Gudlaugsson <gunnar.gudlaugsson@egde.no>
2026-07-20 10:38:40 +0200

    Use org.springframework.util.unit.DataSize to have human readable configuration setting

--- src/ext/java/no/entur/uttu/ext/entur/organisation/client/NetexOrganisationRegistryClient.java
+++ src/ext/java/no/entur/uttu/ext/entur/organisation/client/NetexOrganisationRegistryClient.java
@@ -1,10 +1,10 @@
 package no.entur.uttu.ext.entur.organisation.client;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.util.unit.DataSize;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -12,31 +12,21 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Profile("entur-netex-organisation-registry-client")
 public class NetexOrganisationRegistryClient {
 
-  @Value("${netex.organisations.max-in-memory-size-kb:500}")
-  String maxInMemorySizeKB;
+  @Value("${netex.organisations.max-in-memory-size:500KB}")
+  DataSize maxInMemorySize;
 
   @Bean("orgRegisterClient")
   WebClient webClient(WebClient.Builder webClientBuilder) {
     return webClientBuilder
       .defaultHeader("Et-Client-Name", "entur-nplan")
       .exchangeStrategies(
-        ExchangeStrategies
-          .builder()
-          .codecs(codecs ->
-            codecs.defaultCodecs().maxInMemorySize(resolveMaxInMemorySizeBytes())
+        ExchangeStrategies.builder()
+          .codecs(
+            codecs ->
+              codecs.defaultCodecs().maxInMemorySize((int) maxInMemorySize.toBytes())
           )
           .build()
       )
       .build();
-  }
-
-  private int resolveMaxInMemorySizeBytes() {
-    long maxInMemorySizeKb;
-    try {
-      maxInMemorySizeKb = Long.parseLong(maxInMemorySizeKB);
-    } catch (NumberFormatException e) {
-      maxInMemorySizeKb = 500L;
-    }
-    return (int) (maxInMemorySizeKb * 1024);
   }
 }
